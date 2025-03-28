c2ae294cafd32a8125fd6fec5cfcc5b59f3526ba
willymariino <wg.mariino@gmail.com>
2025-03-28 15:56:57 +0100

    created routers folder

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express") // vado ad importare express
+const app = express() // invoco express
+const port = 3000 // indico quale porta voglio utilizzare
+
+app.use(express.static("public")) // vado ad accedere a risorse statiche come le immagini
+
+app.use("/posts", postRouter)
+
+app.listen(port, () => {
+    console.log("server attivo sulla porta:" + " " + port)
+
+})
