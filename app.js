e8c382442c383c31f98b27d1f1ed3199ac1ef335
willymariino <wg.mariino@gmail.com>
2025-03-28 18:52:17 +0100

    fixed app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express") // vado ad importare express
 const app = express() // invoco express
 const port = 3000 // indico quale porta voglio utilizzare
 
+const postRouter = require("./router/posts")
+
 app.use(express.static("public")) // vado ad accedere a risorse statiche come le immagini
 
 app.use("/posts", postRouter)
